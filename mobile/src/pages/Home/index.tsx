9f95661cef65d728cc50441260c9160a9f50b425
Davi Félix <davi.felix.lima@gmail.com>
2020-06-05 20:41:30 -0300

    Adicionando select dinâmico em UFs e cidades

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,20 +1,76 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text, ImageBackground, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 
 import { RectButton } from 'react-native-gesture-handler'
+import RNPickerSelect from 'react-native-picker-select'
 import { Feather as Icon } from '@expo/vector-icons'
 
 import { useNavigation } from '@react-navigation/native'
 
+import ibge from './../../services/ibge'
+
 import styles from './style'
 
+interface Uf {
+    initials: string,
+    name: string,
+}
+
+interface UfIBGE {
+    sigla: string,
+    nome: string
+}
+
+interface City {
+    name: string
+}
+
+interface CityIBGE {
+    nome: string
+}
+
+
 const Home = () => {
-    const [uf, setUf] = useState('')
-    const [city, setCity] = useState('')
+    const [ufs, setUfs] = useState<Uf[]>([])
+    const [selectedUf, setSelectedUf] = useState<Uf>({} as Uf)
+    const [cities, setCities] = useState<City[]>([])
+    const [selectedCity, setSelectedCity] = useState<City>({} as City)
     const navigation = useNavigation()
 
+    useEffect(() => {
+        ibge.get<UfIBGE[]>('')
+            .then(response => {
+                setUfs(response.data.map(item => {
+                    return {
+                        initials: item.sigla,
+                        name: item.nome
+                    }
+                }))
+            })
+    }, [])
+
+    useEffect(() => {
+        if (JSON.stringify(selectedUf) == '{}') return  // Objeto vazio
+        ibge.get<CityIBGE[]>(`${selectedUf}/municipios`)
+            .then(response => {
+                setCities(response.data.map(city => {
+                    return {
+                        name: city.nome
+                    }
+                }))
+            })
+    }, [selectedUf])
+
     function navigateToPoints() {
-        navigation.navigate('Points', { uf, city })
+        navigation.navigate('Points', { uf: selectedUf, city: selectedCity })
+    }
+
+    function handleSelectUf(value: Uf) {
+        setSelectedUf(value)
+    }
+
+    function handleSelectCity(value: City) {
+        setSelectedCity(value)
     }
 
     return (
@@ -36,22 +92,28 @@ const Home = () => {
                 </View>
 
                 <View style={styles.footer}>
-                    <TextInput
-                        value={uf}
-                        onChangeText={setUf}
-                        maxLength={2}
-                        autoCorrect={false}
-                        style={styles.input}
-                        placeholder="Digite a UF"
-                        autoCapitalize="characters"
+
+                    <RNPickerSelect
+                        items={ufs.map(uf => {
+                            return {
+                                label: `${uf.name} (${uf.initials})`,
+                                value: uf.initials,
+                            }
+                        })}
+                        onValueChange={handleSelectUf}
+                        placeholder={{ label: 'Seleciona uma UF', value: 0 }}
                     />
-                    <TextInput
-                        value={city}
-                        onChangeText={setCity}
-                        style={styles.input}
-                        autoCorrect={false}
-                        placeholder="Digite a cidade"
+                    <RNPickerSelect
+                        items={cities.map(city => {
+                            return {
+                                label: city.name,
+                                value: city.name,
+                            }
+                        })}
+                        onValueChange={handleSelectCity}
+                        placeholder={{ label: 'Seleciona uma cidade', value: '' }}
                     />
+
                     <RectButton style={styles.button} onPress={navigateToPoints}>
                         <View style={styles.buttonIcon}>
                             <Text>
